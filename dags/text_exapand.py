d265ff3ff448e29fe737844e7f9075d9f182437a
YORAM12LUX <desiregbane0@gmail.com>
2025-11-01 12:12:54 +0000

    fix

--- dags/text_exapand.py
+++ dags/text_exapand.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 with DAG(
     dag_id = "my_dag",
-    start_date = datetime(2023, 1, 1),
+    start_date = datetime(2023, 1, 1), 
     scheule_interval = "@daily",
     catchup = False,
 ) as dag:
